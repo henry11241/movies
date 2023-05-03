c4ccc453c83ad29dd1c237c574b6778616f4f637
Henry Lu <henry11241@gmail.com>
2023-05-03 15:52:13 +0800

    make a detail movie render page

--- index.js
+++ index.js
@@ -51,6 +51,7 @@ const onInput = async event => {
     option.addEventListener('click', () => {
       dropdown.classList.remove('is-active')
       input.value = movie.Title
+      onMovieSelect(movie)
     })
 
     resultsWrapper.appendChild(option)
@@ -64,3 +65,53 @@ document.addEventListener('click', event => {
     dropdown.classList.remove('is-active')
   }
 })
+
+const onMovieSelect = async movie => {
+  const response = await axios.get('http://www.omdbapi.com/', {
+    params: {
+      apikey: 'a4091f84',
+      i: movie.imdbID
+    }
+  })
+
+  document.querySelector('#summary').innerHTML = movieTemplate(response.data)
+}
+
+const movieTemplate = (movieDetail) => {
+  return `
+    <div class="media">
+      <figure class="media-left">
+      <p class="image">
+        <img src="${movieDetail.Poster}" />
+      </p>
+      </figure>
+      <div class="media-content">
+        <div class="content">
+          <h1>${movieDetail.Title}</h1>
+          <h4>${movieDetail.Genre}</h4>
+          <p>${movieDetail.Plot}</p>
+        </div>
+      </div>
+    </div>
+    <div class="notification is-primary"> 
+      <p class="title">${movieDetail.Awards}</p>
+      <p class="subtitle">Awards</p>
+    </div>
+    <div class="notification is-primary">
+      <p class="title">${movieDetail.BoxOffice}</p>
+      <p class="subtitle">Box Office</p>
+    </div>
+    <div class="notification is-primary">
+      <p class="title">${movieDetail.Metascore}</p>
+      <p class="subtitle">Metascore</p>
+    </div>
+    <div class="notification is-primary">
+      <p class="title">${movieDetail.imdbRating}</p>
+      <p class="subtitle">IMDB Rating</p>
+    </div>
+    <div class="notification is-primary">
+      <p class="title">${movieDetail.imdbVotes}</p>
+      <p class="subtitle">IMDB Votes</p>
+    </div>
+  `
+} 
